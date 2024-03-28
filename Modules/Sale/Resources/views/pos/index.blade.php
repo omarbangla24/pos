b5ce81042dae2c9edf1a741da95960fc9ba52d51
omarbangla24 <omarbangla24@gmail.com>
2024-03-29 01:03:24 +0600

    first commit

---
+++ Modules/Sale/Resources/views/pos/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('title', 'POS')
+
+@section('third_party_stylesheets')
+
+@endsection
+
+@section('breadcrumb')
+    <ol class="breadcrumb border-0 m-0">
+        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+        <li class="breadcrumb-item active">POS</li>
+    </ol>
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                @include('utils.alerts')
+            </div>
+            <div class="col-lg-7">
+                <livewire:search-product/>
+                <livewire:pos.product-list :categories="$product_categories"/>
+            </div>
+            <div class="col-lg-5">
+                <livewire:pos.checkout :cart-instance="'sale'" :customers="$customers"/>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('page_scripts')
+    <!-- No MaskMoney script here -->
+    <script>
+        $(document).ready(function () {
+            window.addEventListener('showCheckoutModal', event => {
+                $('#checkoutModal').modal('show');
+            });
+        });
+    </script>
+@endpush
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Logicean POS © {{ date('Y') }} | Developed by <strong><a target="_blank" href="https://logicean.com">Logicean</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">3.0</strong></div>
+</footer>
